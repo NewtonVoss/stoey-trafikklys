0c39a74798a1e39d0b5505de484d463e8d92deea
NewtonVoss <77486794+NewtonVoss@users.noreply.github.com>
2021-05-27 14:38:01 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 led.enable(false)
 let strip = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
-OLED.init(128, 64)
 basic.forever(function () {
     if (smarthome.ReadNoise(AnalogPin.P2) > 110) {
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
@@ -9,7 +8,4 @@ basic.forever(function () {
     } else {
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
     }
-})
-basic.forever(function () {
-    OLED.writeNumNewLine(smarthome.ReadNoise(AnalogPin.P1))
 })
